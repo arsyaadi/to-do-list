30a46c411fb809d7cab9d7414bfda28385b4a7ab
arsyaadi <arsyaadi@gmail.com>
2022-05-04 22:31:51 +0700

    add readme,change ring button

--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -6,7 +6,7 @@ export default function Input({handleSubmit, handleChange, todoItem}) {
   return (
     <div>
         <input className='h-9 w-full my-5 focus:outline-none focus:ring-2 focus:ring-hijau rounded border border-slate-400' type="text" name='todoItem' placeholder=' Input something' value={todoItem} onChange={(e) => {handleChange(e)}}/>
-        <button className='bg-hijau px-4 py-1 ring-2 ring-hijau rounded-md text-slate-800' type='submit ' onClick={() => handleSubmit()} >Add</button>
+        <button className='bg-hijau px-4 py-1 ring-1 ring-green-700 rounded-md text-slate-800' type='submit ' onClick={() => handleSubmit()} >Add</button>
     </div>
   )
 }
